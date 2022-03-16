f1167652dd89f6c06794a761065a81805bf63c76
ManuOmulo <omuloemmanuel@gmail.com>
2022-03-16 23:56:20 +0300

    🖇  Added share links

--- pages/articles/[articleId].js
+++ pages/articles/[articleId].js
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import Head from 'next/head'
+import Script from 'next/script'
 import Link from 'next/link'
 import Image from 'next/image'
 import useSWR from 'swr'
 import ReactMarkdown from 'react-markdown'
 import Header from '../../components/header/header'
+import { BsFacebook, BsLinkedin } from "react-icons/bs"
+import { FaWhatsappSquare, FaTwitterSquare } from 'react-icons/fa'
 
 import styles from '../../styles/SingleArticle.module.scss'
 
@@ -12,7 +15,7 @@ const Article = ({ article }) => {
   const [suggestionList, setSuggestionList] = useState([])
 
   const { data, error } = useSWR("allArticles", async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/articles?sort=id:desc&fields=title,type,description,publishedAt&filters[type][$eq]=${article.attributes.type}&pagination[limit]=5&populate[image][fields][0]=url`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/articles?sort=id:desc&fields=title,type,description,publishedAt&filters[type][$eq]=${article.attributes.type}&pagination[limit]=4&populate[image][fields][0]=url`)
     const data = await response.json()
     const filtered = data.data.filter(item => item.id !== article.id)
     setSuggestionList(filtered)
@@ -35,16 +38,23 @@ const Article = ({ article }) => {
     </div>
   ))
 
+  function openWhatsApp() {
+    window.open('https://web.whatsapp.com://send?text= https://www.youtube.com/watch?v=ohpCMpderow');
+  }
+
+
   return (
     <main>
       <Head>
         <meta property="og:title" content={`${article.attributes.title}`}/>
         <meta property="og:image" content={`${article.attributes.image.data.attributes}`}/>
-        <meta property="og:description" content="DESCRIPTION OF YOUR SITE"/>
+        <meta property="og:description" content={`${article.attributes.description.slice(0, 50)}`}/>
         <meta property="og:url" content="URL OF YOUR WEBSITE"/>
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="627" />
+        <meta property="og:type" content='website'/>
       </Head>
+      <Script async src="https://platform.twitter.com/widgets.js" charset="utf-8"/>
 
       <Header title={article.attributes.title} item={article.attributes.image.data.attributes.url}/>
 
@@ -72,16 +82,28 @@ const Article = ({ article }) => {
             <h3>Share this Article</h3>
 
             <div className={styles.links}>
-              <a target="_blank" rel="noreferrer" href={"https://twitter.com/share?url=<URL>&text=<TEXT>via=<USERNAME>"}>
-                Twitter
+              <a target="_blank" rel="noreferrer" href={`https://twitter.com/intent/tweet?text=${article.attributes.title}&url=https://voirme.co.ke/article/${article.id}`}>
+                <FaTwitterSquare className={styles.twitter} style={{color: "#1A83CC"}}/>
               </a>
 
-              <a target="_blank" rel="noreferrer" href={"https://www.facebook.com/sharer/sharer.php?u=<URL>"}>
-                Facebook
+              <a
+                href={`https://www.facebook.com/sharer/sharer.php?u=https://voirme.co,ke/articles/${article.id}`}
+                target="_blank"
+                rel="noreferrer">
+                <BsFacebook className={styles.facebook} style={{ color: "#0080F6"}}/>
               </a>
 
-              <a target="_blank" rel="noreferrer" href={"mailto:?subject=<SUBJECT>&body=<BODY>"}>
-                Email
+              <a href={`whatsapp://send?text=${article.attributes.title}`}        data-action="share/whatsapp/share"
+              target="_blank" rel="noreferrer" >
+                <FaWhatsappSquare className={styles.whatsapp} style={{ color: "#25D366"}}/>
+              </a>
+
+              <a
+                href={`https://www.linkedin.com/sharing/share-offsite/?url=https://voirme.co.ke/article/${article.id}`}
+                target="_blank"
+                rel="noreferrer"
+                >
+                <BsLinkedin className={styles.linkedIn} style={{color: "blue"}}/>
               </a>
             </div>
           </div>
