23ae02f2d1d3b6559ed3076902da885fe24fd628
ManuOmulo <omuloemmanuel@gmail.com>
2022-03-25 19:11:02 +0300

    🛒 Shop Page

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,12 +1,58 @@
-import Header from '../../components/header/header'
+import Image from 'next/image'
+import { BsInstagram, BsLinkedin, BsTwitter, BsFacebook } from 'react-icons/bs'
+
+import Navbar from '../../components/navbar/navbar'
+import Socials from '../../components/socials/socials'
+import banner from '../../public/coming_soon.webp'
+
+import styles from '../../styles/Shop.module.scss'
 
 const Shop = () => {
   return (
-    <div>
-      <Header />
+    <div className={styles.main}>
+      <Navbar />
 
-      <section>
-        <h2>Shop Here</h2>
+      <section className={styles.container}>
+        <div className={styles.banner}>
+          <Image src={banner} className={styles.image} alt="coming soon" />
+        </div>
+        <div className={styles.details}>
+          <div className={styles.headerContainer}>
+            <div className={styles.header}>
+              <h1>Coming Soon</h1>
+            </div>
+          </div>
+          <div className={styles.socials}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href=""
+            >
+              <BsInstagram />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href=""
+            >
+              <BsFacebook />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href=""
+            >
+              <BsTwitter />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href=""
+            >
+              <BsLinkedin />
+            </a>
+          </div>
+        </div>
       </section>
     </div>
   )
